c5466fab9212eb30d3fe9ed841e1792c9110df33
kamilpinas <kamilpinas@gmail.com>
2021-05-31 09:36:43 +0200

    p

--- app/REST/routes.js
+++ app/REST/routes.js
@@ -1,7 +1,9 @@
 import userEndpoint from './user.endpoint';
+import userEndpoint from './post.endpoint';
 
-const routes = function (router) {
+const routes = function(router) {
     userEndpoint(router);
+    postEndpoint(router);
 };
 
 export default routes;
